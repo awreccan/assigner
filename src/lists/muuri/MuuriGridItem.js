fbf52f881746897e8e7292371a7e35eb99753c9f
awreccan <sortilegesam@gmail.com>
2018-03-26 15:50:01 +0800

    fix "this.props.setRef is not a function" [issues: 2 (-1/+1)]
    error occurs because MuuriGridItem is also being called by List, which
    does not pass a setRef prop

--- src/lists/muuri/MuuriGridItem.js
+++ src/lists/muuri/MuuriGridItem.js
@@ -2,9 +2,13 @@ import React, { Component } from 'react'
 import './Muuri.css'
 
 export default class MuuriGridItem extends Component {
+  setRef(r) {
+    this.props.setRef && this.props.setRef(r)
+  }
+
   render() {
     return (
-      <div {...this.props} ref={r => this.props.setRef(r)}>
+      <div {...this.props} ref={r => this.setRef(r)}>
         <div className='muuri-item-content'>
           { this.props.children }
         </div>
